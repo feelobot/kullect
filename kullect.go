fb732765ee08f72be62296e0aba164a3f6573d57
feelobot <felix.a.rod@gmail.com>
2016-08-29 14:32:25 +0200

    working

--- kullect.go
+++ kullect.go
@@ -11,7 +11,6 @@ import (
 
 // An Agent.Handler
 type costHandler struct {
-	as           string
 	hourly_cost  int
 	total_cpu    int
 	total_memory int
@@ -28,10 +27,9 @@ func (a *costHandler) Info() (*udf.InfoResponse, error) {
 		Wants:    udf.EdgeType_STREAM,
 		Provides: udf.EdgeType_STREAM,
 		Options: map[string]*udf.OptionInfo{
-			"as":           {ValueTypes: []udf.ValueType{udf.ValueType_STRING}},
 			"total_cpu":    {ValueTypes: []udf.ValueType{udf.ValueType_INT}},
 			"total_memory": {ValueTypes: []udf.ValueType{udf.ValueType_INT}},
-			"hourly_cost":  {ValueTypes: []udf.ValueType{udf.ValueType_INT}},
+			"hourly_cost":  {ValueTypes: []udf.ValueType{udf.ValueType_DOUBLE}},
 		},
 	}
 	return info, nil
@@ -45,22 +43,16 @@ func (a *costHandler) Init(r *udf.InitRequest) (*udf.InitResponse, error) {
 	}
 	for _, opt := range r.Options {
 		switch opt.Name {
-		case "as":
-			a.as = opt.Values[0].Value.(*udf.OptionValue_StringValue).StringValue
 		case "hourly_cost":
-			a.as = opt.Values[0].Value.(*udf.OptionValue_IntValue).IntValue
+			a.hourly_cost = int(opt.Values[0].Value.(*udf.OptionValue_DoubleValue).DoubleValue)
 		case "total_cpu":
-			a.as = opt.Values[0].Value.(*udf.OptionValue_IntValue).IntValue
+			a.total_cpu = int(opt.Values[0].Value.(*udf.OptionValue_IntValue).IntValue)
 		case "total_memory":
-			a.as = opt.Values[0].Value.(*udf.OptionValue_IntValue).IntValue
+			a.total_memory = int(opt.Values[0].Value.(*udf.OptionValue_IntValue).IntValue)
 		}
 	}
 
-	if a.as == "" {
-		init.Success = false
-		init.Error += " invalid as name provided"
-	}
-	if a.hourly_cost == 0 {
+	if a.hourly_cost == 0.0 {
 		init.Success = false
 		init.Error += " must supply the hourly cost of your entire kubernetes cluster"
 	}
@@ -70,7 +62,7 @@ func (a *costHandler) Init(r *udf.InitRequest) (*udf.InitResponse, error) {
 	}
 	if a.total_memory == 0 {
 		init.Success = false
-		init.Error += " must supply the total available memory in your cluster"
+		init.Error += " must supply the total available memory  in megabytes from your cluster"
 	}
 	return init, nil
 }
@@ -96,10 +88,13 @@ func (a *costHandler) BeginBatch(*udf.BeginBatch) error {
 func (a *costHandler) Point(p *udf.Point) error {
 	// Re-use the existing point so we keep the same tags etc.
 	cpu_usage := float64(p.FieldsInt["cpu.value"])
+	memory_usage := float64(p.FieldsInt["memory.value"])
 	uptime := float64(p.FieldsInt["uptime.value"])
 	hourly_uptime := uptime / 36000000
-	cost := (hourly_uptime * float64(a.hourly_cost)) * (cpu_usage / float64(a.total_cpu))
-	p.FieldsDouble = map[string]float64{a.as: cost}
+	uptime_cost := hourly_uptime * float64(a.hourly_cost)
+	cpu_cost := uptime_cost * (cpu_usage / float64(a.total_cpu))
+	memory_cost := uptime_cost * (memory_usage / float64(a.total_memory))
+	p.FieldsDouble = map[string]float64{"cpu_cost": cpu_cost, "memory_cost": memory_cost}
 	p.FieldsInt = nil
 	p.FieldsString = nil
 	// Send point with average value.
